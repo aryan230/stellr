741fcb3d25ed20ae3926f809b1c1d48fb2ab8d1c
aryan230 <aryan23062001@gmail.com>
2023-09-06 20:17:25 +0530

    admin-users-data-fix-main

--- src/Admin/Metrics/AUsers.jsx
+++ src/Admin/Metrics/AUsers.jsx
@@ -6,7 +6,7 @@ function AUsers({ data }) {
   return (
     <div className="relative overflow-x-auto px-10 h-[80%]">
       <table className="w-full text-sm text-left text-gray-500 h-[100%]">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 sticky top-0">
           <tr>
             <th scope="col" className="px-6 py-3">
               ID
